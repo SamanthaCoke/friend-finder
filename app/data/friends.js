b80c7cbd97ec71d8d4c45b4064d9fd419d7bf7ba
SamanthaCoke <57117836+SamanthaCoke@users.noreply.github.com>
2020-05-26 13:57:01 -0600

    created survery page

---
+++ app/data/friends.js
@@ -0,0 +1,123 @@
+let friends = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"Ashley",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            2,
+            5,
+            3,
+            4,
+            5,
+            1,
+            4,
+            5,
+            3
+          ]
+      
+      },
+
+      {
+      "name":"Sara",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            2,
+            3,
+            1,
+            4,
+            5,
+            5,
+            2,
+            3,
+            4
+          ]
+        },
+
+        {
+
+        "name":"Saria",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            2,
+            1,
+            5,
+            4,
+            2,
+            1,
+            4,
+            3,
+            5
+          ]
+        },
+        {
+
+        "name":"Raine",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            2,
+            1,
+            3,
+            1,
+            4,
+            2,
+            3,
+            5,
+            4
+          ]
+        },
+        {
+
+        "name":"Caleb",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            2,
+            1,
+            1,
+            2,
+            3,
+            4,
+            5,
+            4,
+            5
+          ]
+        },
+
+        {
+            "name":"Isaiah",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            2,
+            2,
+            1
+          ]
+        },
+      
+      
+]
